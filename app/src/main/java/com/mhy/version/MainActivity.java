6c956b886c6f8b3602013937988f5ea4e9b53d4b
1976222027 <2010MA520ma>
2019-08-20 21:53:22 +0800

    add

--- app/src/main/java/com/mhy/version/MainActivity.java
+++ app/src/main/java/com/mhy/version/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import com.mhy.version.v1.V1Activity;
 import com.mhy.version.v2.V2Activity;
+import com.mhy.versionlib.v2.AllenVersionChecker;
+import com.mhy.versionlib.v2.builder.UIData;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void checkUpdata() {
+        AllenVersionChecker
+                .getInstance()
+                .downloadOnly(UIData.create()
+                        .setDownloadUrl("http://test-1251233192.coscd.myqcloud.com/1_1.apk")//"apk下载地址"
+                        .setTitle("版本更新")
+                        .setContent("点击下载新版本")
+                ).executeMission(MainActivity.this);
 
+
+    }
     public void mainOnClick(View view) {
         Intent intent;
         switch (view.getId()) {
-            case R.id.btn_v1:
+            case R.id.btn_v0:
+                checkUpdata();
+                break;
+                case R.id.btn_v1:
                 intent = new Intent(this, V1Activity.class);
                 startActivity(intent);
+
                 break;
             case R.id.btn_v2:
                 intent = new Intent(this, V2Activity.class);
